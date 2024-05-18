e916566107934d43b745173b787294d77780c925
Rahul Ramakrishnan <151418520+RahulRamakrish@users.noreply.github.com>
2024-05-17 21:45:39 -0400

    Update and rename menu.py to creating distortion node in nuke/menu.py

--- menu.py
+++
@@ -1,5 +0,0 @@
-toolbar = nuke.toolbar("Nodes")
-
-ceaMenu = toolbar.addMenu("CEA", icon="cea24x24.png")
-
-ceaMenu.addCommand("Gizmos/CEA_Gizmos/New_Distortion", "nuke.createNode(\"New_Distortion\")", icon="distort.png")
